8f084b84c20a4c0c6237f62c30f809394e7eb501
sekarasriii <sekareeesh@gmail.com>
2026-01-02 14:17:30 +0700

    menambahkan deginisi kontrak fungsi get data siswa dan post data siswa yg diimplementasikan untuk manajemen data siswa

---
+++ app/src/main/java/com/example/meet14_firebase/repositori/RepositorySiswa.kt
@@ -0,0 +1,10 @@
+package com.example.meet14_firebase.repositori
+
+import com.example.meet14_firebase.modeldata.Siswa
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.coroutines.tasks.await
+
+interface RepositorySiswa {
+    suspend fun getDataSiswa(): List<Siswa>
+    suspend fun postDataSiswa(siswa: Siswa)
+}
