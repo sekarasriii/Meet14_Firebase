e19c7af700ce2ad3375fe23c5a22ff66c0660b19
sekarasriii <sekareeesh@gmail.com>
2026-01-02 15:16:10 +0700

    menambahkan destinasi home sebagai implementasi destinasi navigasi

---
+++ app/src/main/java/com/example/meet14_firebase/view/route/DestinasiHome.kt
@@ -0,0 +1,8 @@
+package com.example.meet14_firebase.view.route
+
+import com.example.meet14_firebase.R
+
+object DestinasiHome: DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
