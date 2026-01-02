3b30c555523df20f99c22aafb5371c95a3fcffb0
sekarasriii <sekareeesh@gmail.com>
2026-01-02 17:01:30 +0700

    menyelesaikan code

--- app/src/main/java/com/example/meet14_firebase/MainActivity.kt
+++ app/src/main/java/com/example/meet14_firebase/MainActivity.kt
@@ -7,10 +7,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.meet14_firebase.ui.theme.Meet14_FirebaseTheme
 import com.example.meet14_firebase.view.controllNavigasi.DataSiswaApp
 
@@ -27,21 +24,5 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Meet14_FirebaseTheme {
-        Greeting("Android")
     }
 }
