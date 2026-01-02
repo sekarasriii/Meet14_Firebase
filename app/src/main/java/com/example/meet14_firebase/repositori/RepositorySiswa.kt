62fcbf5d1c409aa0d57288e252a54c05bd10aa33
sekarasriii <sekareeesh@gmail.com>
2026-01-02 14:18:28 +0700

    menambahkan kelas implementasi repository yg terhubung langsung ke database cloud firestore

--- app/src/main/java/com/example/meet14_firebase/repositori/RepositorySiswa.kt
+++ app/src/main/java/com/example/meet14_firebase/repositori/RepositorySiswa.kt
@@ -8,3 +8,6 @@ interface RepositorySiswa {
     suspend fun getDataSiswa(): List<Siswa>
     suspend fun postDataSiswa(siswa: Siswa)
 }
+class FirebaseRepositorySiswa : RepositorySiswa {
+    private val db = FirebaseFirestore.getInstance()
+    private val collection = db.collection("siswa")
