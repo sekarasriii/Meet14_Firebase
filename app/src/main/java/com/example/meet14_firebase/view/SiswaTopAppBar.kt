d608b5ce00dc47ab5de488c8f35792aa469a6a49
sekarasriii <sekareeesh@gmail.com>
2026-01-02 15:27:36 +0700

    tambah definisi composable siswatopappbar

---
+++ app/src/main/java/com/example/meet14_firebase/view/SiswaTopAppBar.kt
@@ -0,0 +1,24 @@
+package com.example.meet14_firebase.view
+
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.TopAppBarScrollBehavior
+import androidx.compose.material3.Text
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import com.example.meet14_firebase.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SiswaTopAppBar(
+    title: String,
+    canNavigateBack: Boolean,
+    modifier: Modifier = Modifier,
+    scrollBehavior: TopAppBarScrollBehavior? = null,
+    navigateUp: () -> Unit = {}
+)
