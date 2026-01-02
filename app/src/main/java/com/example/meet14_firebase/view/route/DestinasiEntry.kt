2f0e5cb94e995976deedfb272e54c31eaadc3b3d
sekarasriii <sekareeesh@gmail.com>
2026-01-02 15:15:21 +0700

    menambahkan destinasi entry sebagai implementasi destinasi navigasi

---
+++ app/src/main/java/com/example/meet14_firebase/view/route/DestinasiEntry.kt
@@ -0,0 +1,8 @@
+package com.example.meet14_firebase.view.route
+
+import com.example.meet14_firebase.R
+
+object DestinasiEntry: DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
